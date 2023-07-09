519a89580a26a63ffd3909e4029336d90b430ed0
inseo <wisberry2022@gmail.com>
2023-07-09 15:19:04 +0900

    07/09 upload [main][update] build test

--- web/mysalary/src/main/java/com/account/mysalary/controller/AccountController.java
+++ web/mysalary/src/main/java/com/account/mysalary/controller/AccountController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("mysalary")
 public class AccountController {
 
     private final AccountService accountService;
--- web/mysalary/src/main/java/com/account/mysalary/controller/DebitController.java
+++ web/mysalary/src/main/java/com/account/mysalary/controller/DebitController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("mysalary")
 public class DebitController {
 
     private final DebitService debitService;
