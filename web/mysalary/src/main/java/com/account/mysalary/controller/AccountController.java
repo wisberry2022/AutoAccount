620d8027be4dffc76cef680e57d74ef5ab57ed08
wisberry2022 <wisberry2022@gmail.com>
2023-07-11 20:56:25 +0900

    07/11 upload [main][hotfix] boot server cors fixed

--- web/mysalary/src/main/java/com/account/mysalary/controller/AccountController.java
+++ web/mysalary/src/main/java/com/account/mysalary/controller/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("mysalary")
